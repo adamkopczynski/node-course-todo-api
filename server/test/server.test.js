9e3a7917189907cb3a102dc36a8ea6aac55063e1
Adam <akadamkopczynski@gmail.com>
2018-07-09 19:44:30 +0200

    Add DELETE /todos/:id tests

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -104,4 +104,41 @@ describe('GET /todos/:id', () => {
         .end(done)
     })
 })
+
+describe('DELETE /todos/:id', () => {
+    it('should delete todo with id', (done) => {
+        const id = todos[0]._id.toHexString();
+
+        request(app)
+        .delete(`/todos/${id}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end((err, res) => {
+            if(err) return done(err);
+            
+            Todo.findOne({id})
+            .then((todo) => {
+                expect(todo).toBeFalsy();
+                done();
+            })
+            .catch((e) => done(e))
+        })
+    })
+
+    it('should return 404 if todo not found', (done) => {
+        request(app)
+        .delete(`/todos/${new ObjectID().toHexString()}`)
+        .expect(404)
+        .end(done)
+    })
+
+    it('should return 404 for non-object ids', (done) => {
+        request(app)
+        .delete(`/todos/123`)
+        .expect(404)
+        .end(done)
+    })
+})
  
