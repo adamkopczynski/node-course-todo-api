58b7fa44d38410a5fbdaf5fee75ff912e852161a
Adam <akadamkopczynski@gmail.com>
2018-07-10 23:50:37 +0200

    Add POSOST /users route and JWT

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,54 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-const User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
     email: {
         required: true,
         type: String,
         minlength: 1,
-        trim: true
-    }
-})
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is invalid email.'
+        }
+    },
+    password:{
+        require: true,
+        type: String,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            require: true
+        },
+        token:{
+            type: String,
+            require: true
+        }
+    }]
+});
+
+UserSchema.methods.generateAuthToken = function() {
+    const user = this;
+    const access = 'auth';
+    const token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123');
+
+    user.tokens = user.tokens.concat([{access, token}]);
+
+    user.save().then(() => ({token}));
+}
+
+UserSchema.methods.toJSON = function() {
+    const user = this;
+    const userObject = user.toObject();
+
+    return _.pick(user, ['_id', 'email']);
+}
+
+const User = mongoose.model('User', UserSchema)
 
 module.exports = {User}
